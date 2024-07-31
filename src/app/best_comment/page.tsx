0df02439f05901cfbbbf955a51a2a6c76afa9101
sehxxnee <shyan5253@naver.com>
2024-07-31 22:52:15 +0900

    Refactor: best_comment page에서 comments 데이터 분리, data 폴더 아래로 이동

--- src/app/best_comment/page.tsx
+++ src/app/best_comment/page.tsx
@@ -6,22 +6,8 @@ import GridLayout from '../../components/layout/GridLayout';
 import AuthBox from '../../components/auth/AuthBox'; 
 import MessageBox from '../../components/grid/MessageBox';
 import CommentLayout from '../../components/layout/CommentLayout';
-
-const comments = [
-  {
-    image: 'https://via.placeholder.com/150',
-    title: '기사 제목',
-    description: '언론사, 발행일자',
-    content: '베스트댓글 내용',
-  },
-  {
-    image: 'https://via.placeholder.com/150',
-    title: '기사 제목',
-    description: '언론사, 발행일자',
-    content: '베스트댓글 내용',
-  },
-  // Add more comments as needed
-];
+import {comments} from '../../data/comments';
+ 
 
 export default function Home() { 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
---
+++ src/data/comments.ts
@@ -0,0 +1,14 @@
+export const comments = [
+    {
+      image: 'https://via.placeholder.com/150',
+      title: '기사 제목',
+      description: '언론사, 발행일자',
+      content: '베스트댓글 내용',
+    },
+    {
+      image: 'https://via.placeholder.com/150',
+      title: '기사 제목',
+      description: '언론사, 발행일자',
+      content: '베스트댓글 내용',
+    }, 
+  ];
